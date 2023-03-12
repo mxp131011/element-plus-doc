17170806de565ec9329b56801850edc03183ce72
O昵称重要吗O <mxp131011@qq.com>
2023-03-12 21:11:12 +0800

    s

---
+++ src/text/pages/index/index copy.ts
@@ -0,0 +1,11 @@
+import type { TagDocOld } from '@/types/tag-doc-old';
+
+const attributes: TagDocOld.Attribute[] = [];
+
+const slots: TagDocOld.Slot[] = [];
+
+const exposes: TagDocOld.Expose[] = [];
+
+const documentInstance: TagDocOld.TagDocInstance = { attributes, slots, exposes };
+
+export default documentInstance;
